5024910a2ed5f864e25b78b6fabe90e5ca2cb672
AtefAlmasry <atef_mohamed_@outlook.com>
2022-09-17 18:45:49 +0200

    Add Custom Column to asp Tb

---
+++ TravelTo/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelTo.Controllers
+{
+    public class UserController : Controller
+    {
+        UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register()
+        {
+
+            //_userManager.CreateAsync();
+
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ TravelTo/Models/UserModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TravelTo.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Pass { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+      
+
+    }
+}
